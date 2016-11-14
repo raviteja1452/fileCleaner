b8ed2abfc93cee67b523d7032c4296c830abfba4
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-14 17:23:04 +0530

    added cleaner code

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import ConfigParser
-import os,sys
+import os, sys, time
+from stat import * # ST_SIZE etc
 
-def folderList(path):
-	
+def folderList(path,level,days):
+	if os.path.isdir(path) == True:
+		l =  os.listdir(path)
+		path = path + '/'
+		for i in l:
+			if os.path.isfile(path+i) == True :
+				print level,'File: ',i
+				st = os.stat(path+i)
+				ti = time.time() - st[ST_ATIME]
+				print ti
+				print days*60
+				if(ti > days*60):
+					print 'file to be removed'
+			elif os.path.isdir(path+i) == True :
+				print level,'Directory: ',i
+				folderList(path+i,level+'---',days)
+			else:
+				print level,'Unknown: ',i
+
+
 if __name__ == '__main__':
 	config = ConfigParser.ConfigParser()
 	config.read('fileCleaner.ini')
 	print config.sections()
 	folderName =  config.get('main','foldername')
-
-	# Printing the Contents of the folder
-	l =  os.listdir(folderName)
-	path = folderName+'/'
-	print l
-	for i in l:
-		if os.path.isfile(path+i) == True :
-			print i,'is a file'
-		elif os.path.isdir(path+i) == True :
-			print i,'is a directory'
-		else:
-			print i,'is a unknown'
+	days = config.get('main','daysback')
+	folderList(folderName,'---',days)
