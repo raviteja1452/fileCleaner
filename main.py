a080f821752e7eb4b9c96dcf7071b902a8def69b
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-14 14:55:54 +0530

    progress

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import ConfigParser
 import os,sys
 
-config = ConfigParser.ConfigParser()
+def folderList(path):
+	
+if __name__ == '__main__':
+	config = ConfigParser.ConfigParser()
+	config.read('fileCleaner.ini')
+	print config.sections()
+	folderName =  config.get('main','foldername')
 
-config.read('fileCleaner.ini')
-print config.sections()
-print config.get('main','foldername')
-print config.get('main','daysback')
+	# Printing the Contents of the folder
+	l =  os.listdir(folderName)
+	path = folderName+'/'
+	print l
+	for i in l:
+		if os.path.isfile(path+i) == True :
+			print i,'is a file'
+		elif os.path.isdir(path+i) == True :
+			print i,'is a directory'
+		else:
+			print i,'is a unknown'
